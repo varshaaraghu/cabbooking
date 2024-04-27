75abb523602da3a83a2d590372ec333a4e88aced
varshaaraghu <r.varshaa17@gmail.com>
2024-04-27 18:00:47 +0530

    microservice
    microservice

---
+++ accountmicroservice/src/test/java/com/accountMS/accountmicroservice/AccountmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accountMS.accountmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
